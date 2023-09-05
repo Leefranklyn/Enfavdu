5cb8b204baa321896188b713f0583289706f7eb8
chiamaka28 <chiamakamaduike@gmail.com>
2023-09-05 13:17:05 +0100

    added a succes modal for creating a new password

--- src/componenets/admin/passwordreset/index.jsx
+++ src/componenets/admin/passwordreset/index.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import ConfirmEmail from './ConfirmEmail';
 import Otp from './Otp.JSX';
 import CreateNewPassword from './CreateNewPassword';
+import SuccessModal from './SuccessModal';
 
 const ResetPassword = () => {
   return (
+    <>
+   
     <div className='h-[100vh] bg-lightGrey'>
       <ConfirmEmail/>
+      {/* {<SuccessModal/>} */}
       {/* <Otp/> */}
       {/* <CreateNewPassword/> */}
     </div>
+    </>
   )
 }
 
